a015d61782d0e438196978673cbc49313d311ed5
Kush Jain <Mr.KushJain@gmail.com>
2013-08-02 21:45:55 +0530

    Test for binary comparision and then decimal value

---
+++ src/Number.java
@@ -0,0 +1,53 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kush
+ * Date: 02/08/13
+ * Time: 8:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Number {
+
+    public static final int BINARY_BASE  = 2;
+    public static final int REQUIRED_BIT = 1;
+    private int decimalValue;
+
+    public Number(int decimalValue) {
+        this.decimalValue = decimalValue;
+    }
+
+    public Number() {
+    }
+
+    public int getNumberOfOnes() {
+        int numberOfOnes=0;
+        int givenDecimalNumber=decimalValue;
+        while(givenDecimalNumber!=0){
+             if(givenDecimalNumber%BINARY_BASE == REQUIRED_BIT){
+                 numberOfOnes++;
+             }
+            givenDecimalNumber=givenDecimalNumber/BINARY_BASE;
+        }
+        return numberOfOnes;
+    }
+    public int [] sort(int numberArray[])
+    {
+        for(int i=0;i<numberArray.length;i++)
+        {
+            int j,currentNumber = numberArray[i];
+            for(j=i-1;j>=0 && comparisonCriteria(numberArray[j],currentNumber);j--)
+                numberArray[j+1]=numberArray[j];
+            numberArray[j+1]=currentNumber;
+        }
+        return numberArray;
+    }
+
+    private boolean comparisonCriteria(int arrayNumber,int currentNumber) {
+        if(new Number(arrayNumber).getNumberOfOnes() > new Number(currentNumber).getNumberOfOnes())
+            return true;
+        else if(arrayNumber > currentNumber)
+            return true;
+        return false;
+    }
+
+
+}
---
+++ tests/NumberTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kush
+ * Date: 02/08/13
+ * Time: 8:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NumberTest {
+    @Test
+    public void testOneHasOnlyOneOnes(){
+        assertEquals(1,new Number(1).getNumberOfOnes());
+    }
+
+    @Test
+    public void testTwoHasOnlyOneOnes(){
+        assertEquals(1,new Number(2).getNumberOfOnes());
+    }
+
+    @Test
+    public void testThreeHasTwoOnes(){
+        assertEquals(2,new Number(3).getNumberOfOnes());
+    }
+
+    @Test
+    public void testSortingOfOneGivesOne(){
+        assertArrayEquals(new int [] {1}, new Number().sort(new int[] {1}));
+    }
+
+    @Test
+    public void testSortingOfOneTwoGivesOneTwo(){
+        assertArrayEquals(new int [] {1,2}, new Number().sort(new int[] {1,2}));
+    }
+
+
+    @Test
+    public void testSortingOfTwoOneThreeGivesOneTwoThree(){
+        assertArrayEquals(new int [] {1,2,3}, new Number().sort(new int[] {2,1,3}));
+    }
+
+    @Test
+    public void testSortingOfThreeFourGivesFourThree(){
+        assertArrayEquals(new int [] {4,3}, new Number().sort(new int[] {3,4}));
+    }
+
+}
